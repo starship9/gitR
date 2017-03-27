ac88ca5c09df8d12ffdb22bdb669dec20c52892c
starship9 <nishanksaini10@gmail.com>
2017-03-27 12:16:36 +0530

    Removed debug statements

--- gitR.R
+++ gitR.R
@@ -7,64 +7,64 @@ url<-"https://api.github.com"
 path<-"/users/starship9"
 
 raw.result<-GET(url=url, path=path)
-names(raw.result)
+#names(raw.result)
 
-head(raw.result)
+#head(raw.result)
 
 this.raw.content<-rawToChar(raw.result$content)
-head(this.raw.content)
+#head(this.raw.content)
 
-nchar(this.raw.content)
+#nchar(this.raw.content)
 
 this.json.content<-fromJSON(this.raw.content)
-head(this.json.content)
+#head(this.json.content)
 
 repoURL <- this.json.content$repos_url
 repoRawData<-GET(url=repoURL)
-names(repoRawData)
+#names(repoRawData)
 
-head(repoRawData$content)
+#head(repoRawData$content)
 
 repoContent<-rawToChar(repoRawData$content)
-head(repoContent)
+#head(repoContent)
 
 repoJSON<-fromJSON(repoContent)
-head(repoJSON)
+#head(repoJSON)
 
-class(repoJSON$language)
+#class(repoJSON$language)
 
 repoLanguages<-repoJSON$language
-repoLanguages
-class(repoLanguages)
+#repoLanguages
+#class(repoLanguages)
 
 repoNames<-repoJSON$name
-head(repoNames)
-repoNames
-class(repoNames)
+#head(repoNames)
+#repoNames
+#class(repoNames)
 
 library(dplyr)
 
 repoLanguagesDF<-tbl_df(repoLanguages)
 repoNamesDF<-tbl_df(repoNames)
-class(repoLanguagesDF)
-class(repoNamesDF)
+#class(repoLanguagesDF)
+#class(repoNamesDF)
 
-dim(repoLanguagesDF)
-dim(repoNamesDF)
-length(repoNamesDF)
+#dim(repoLanguagesDF)
+#dim(repoNamesDF)
+#length(repoNamesDF)
 
 #combining the previous data gathered
 repoDataLN<-cbind(repoNamesDF, repoLanguagesDF)
-head(repoDataLN)
+#head(repoDataLN)
 #View(repoDataLN)
-class(repoDataLN)
-repoDataLN
-class(repoDataLN)
-head(repoDataLN)
+#class(repoDataLN)
+#repoDataLN
+#class(repoDataLN)
+#head(repoDataLN)
 colnames(repoDataLN)<-c("Repos","Language")
-repoDataLN
+#repoDataLN
 languageCount<-count(repoDataLN$Language)
-class(languageCount)
+#class(languageCount)
 #repoDataTable<-table(repoDataLN)
 #repoDataTable
 #tbl_df(repoDataTable)
